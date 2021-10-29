d38472f8004380e61e20fbf15a6ef224c72f8ba6
eigen-carmona <jgerardocarg96@gmail.com>
2021-10-29 12:22:28 +0200

    Added validations for upper limit

--- first_program.f90
+++ first_program.f90
@@ -1,15 +1,21 @@
+
 program era_sieve
 
 implicit none
-integer*2 :: N ! The natural number up to which prime numbers will be calculated
-integer*2 :: ii, jj ! indexing variables
+integer*4 :: N ! The natural number up to which prime numbers will be calculated
+integer*4 :: ii, jj ! indexing variables
+integer*4 :: allocate_status
 logical, allocatable :: is_prime(:)
 logical :: index_value, true = 1.gt.0, false = 0.gt.1
 
-write(*,*) "Up to which number do you wish to get primes?"
+write(*,*) "Up to which number do you wish to get primes? Enter an integer N>=2."
 read(*,*) N
 
-allocate(is_prime(2:N))
+! N should be at least 2
+if (N.lt.2) stop "***The upper limit should be at least 2***"
+
+allocate(is_prime(2:N),stat = allocate_status)
+if (allocate_status .ne. 0) stop "***Not enough memory to allocate sieve vector***"
 is_prime = (/(true, ii =2, N)/)
 
 ii = 2
