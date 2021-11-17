d0ff461faa3c21ca91805a79ac8bfabbef6a4880
eigen-carmona <jgerardocarg96@gmail.com>
2021-11-17 20:31:40 +0100

    Python script generates plots

--- week_3/scaling/timing_plot.py
+++ week_3/scaling/timing_plot.py
@@ -1,4 +1,6 @@
-import subprocess, numpy, matplotlib
+import subprocess
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     min_N = input("Enter the minimum number of rows: ")
@@ -7,6 +9,13 @@ def main():
     proc = subprocess.Popen("./matrix_product",stdin = subprocess.PIPE,stdout = subprocess.PIPE)
     proc.stdin.write(f'100,{min_N},{max_N}'.encode('UTF-8'))
     proc.stdin.close()
+    # TODO: adapt for unsuccessful timing scenario
+    '''Importing data from performance.dat'''
+    performance = np.genfromtxt('performance.dat')
+    for i in range(1,4):
+        plot = plt.plot(performance[:,0],performance[:,i])
+        plt.savefig(f'performance_method{i}.jpg')
+    print('Successfully generated plots')
 
 if __name__ == '__main__':
     main()
