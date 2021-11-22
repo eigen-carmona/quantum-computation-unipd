e672b2ddf0541c6736006c848e99d50b71c714f5
eigen-carmona <jgerardocarg96@gmail.com>
2021-11-22 21:47:32 +0100

    Python script fitting complexities

--- week_3/scaling/timing_plot.py
+++ week_3/scaling/timing_plot.py
@@ -1,6 +1,9 @@
 import subprocess
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.optimize import curve_fit
+
+palette = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
 def main():
     min_N = input("Enter the minimum number of rows: ")
@@ -10,11 +13,32 @@ def main():
     proc.stdin.write(f'100,{min_N},{max_N}'.encode('UTF-8'))
     proc.stdin.close()
     # TODO: adapt for unsuccessful timing scenario
+
     '''Importing data from performance.dat'''
     performance = np.genfromtxt('performance.dat')
+    fig, ax = plt.subplots()
+    x = performance[:,0]
     for i in range(1,4):
-        plot = plt.plot(performance[:,0],performance[:,i])
-        plt.savefig(f'performance_method{i}.jpg')
+        '''Leading order term fitting'''
+        f_theo = lambda x, a, b, c: a*x**b + c
+        y = performance[:,i]
+        (a,b,c), _ = curve_fit(f_theo, x, y, maxfev = 2000)
+        f = lambda x: f_theo(x,a,b,c)
+        _, _ax = plt.subplots()
+        _ax.scatter(x,y, color = palette[i])
+        _ax.plot(x,f(x))
+        _ax.set_title(f'Empirical complexity method #{i}')
+        _ax.set_xlabel('input size N')
+        _ax.set_ylabel('computation time (seconds)')
+        power = f'{round(b,2)}'
+        _ax.text(0, max(y),r'Complexity: $\mathcal{O}\left(n^{'+f'{power}'+r'}\right)$', fontsize=10)
+        _.savefig(f'performance_method{i}.jpg')
+        ax.scatter(x,y, color = palette[i])
+    ax.legend([f'method {i}' for i in range(1,4)])
+    ax.set_xlabel('input size N')
+    ax.set_ylabel('computation time (seconds)')
+    ax.set_title('Performance comparison')
+    fig.savefig(f'performances.jpg')
     print('Successfully generated plots')
 
 if __name__ == '__main__':
