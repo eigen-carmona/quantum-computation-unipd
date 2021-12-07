263a9324cb1bde047ab814fd160b8f64daf80ccc
eigen-carmona <jgerardocarg96@gmail.com>
2021-12-07 02:16:46 +0100

    Python script to plot

--- week_5/time_dependent.f90
+++ week_5/time_dependent.f90
@@ -1,4 +1,4 @@
-program phasing_oscillator
+program time_dependent
     ! Compile as ```gfortran time_dependent.f90 -o time_dependent finite_difference.o split_op.o -llapack -Wall -lfftw3 -frecursive```
     ! After compiling ```gfortran -c finite_difference.f90 -llapack``` and ```gfortran -c split_op.f90```
     use finite_diff
@@ -6,26 +6,26 @@ program phasing_oscillator
     
     integer :: NN, ii, n_eigen, timesteps, INFO, N_grid
     real*8, parameter :: L = 10, pi = acos(-1.0)
-    real*8 :: dx, dp, dt = 0.05, norm_sq, T = 5
+    real*8 :: dx, dp, dt, norm_sq, T
     complex*16, allocatable, dimension(:,:) :: Psi
     real*8, allocatable, dimension(:,:) :: psi_states, int_V_t
     real*8, allocatable, dimension(:) :: x_grid, p_grid, energies, V_0
+    character*42 :: states_file
     
 
-    write(*,*) "Enter the target state to evolve. For how many timesteps?"
-    read(*,*) n_eigen, timesteps
+    write(*,*) "Enter the target state to evolve, the timesteps and the constant T"
+    read(*,*) n_eigen, timesteps, T
+    dt = T/timesteps
 
     n_eigen = n_eigen + 1! In our setting, the ground state is shifted to n = 1
 
-    NN = 256!2**8 ! Just because...
+    NN = 2**12 ! Just because...
 
     if (NN.le.n_eigen) stop "The grid size must be higher than the number of energy levels"
 
     ! The grid size determines the increment value
     dx = 2*L/NN
     dp = pi/L
-
-    if (dx.ge.1) stop "Grid size too small. Minumum should be 20 (advised 400 and above)."
     
     ! Since we assume x_0=x_n=0, we reduce the problem to a (NN-1)x(NN-1) matrix
     N_grid = NN - 1
@@ -43,7 +43,7 @@ program phasing_oscillator
     ! Prepare time-evolving integrated potential
     allocate(int_V_t(1:N_grid,1:timesteps))
     do ii = 1, timesteps
-        int_V_t(:,ii) = ((x_grid*T-(ii-1)*dt)**3-(x_grid*T-ii*dt)**3)/(6*T**2)!(T*x_grid-(ii-1)*dt/T)**2/2
+        int_V_t(:,ii) = ((x_grid*T-(ii-1)*dt)**3-(x_grid*T-ii*dt)**3)/(6*T**2)
     end do
 
     ! Calling single particle routine for diagonal potentials and vanishing boundaries
@@ -60,9 +60,10 @@ program phasing_oscillator
     Psi(:,0) = psi_states(:,n_eigen)/sqrt(norm_sq)
     call split_op_1D(Psi,int_V_t,dt,N_grid,p_grid,timesteps,dx)
     ! evolve the state
-    open(12, file = 'states_ev.dat')
+    write(states_file,'(A,I0,A,I0,A,I0,A)') "data/state_",n_eigen-1,"_evo_T",int(T),"_",timesteps,".dat"
+    open(12, file = states_file)
     do ii = 0, timesteps
         write(12,'(*(F0.8,SP,F0.8,"j",","))') Psi(:,ii)
     end do
     close(12)
-end program phasing_oscillator
+end program time_dependent
