e26560d9c056ba06879a9d5565c7c28086528eed
eigen-carmona <jgerardocarg96@gmail.com>
2021-12-16 00:22:25 +0100

    Second version: considerably faster

--- week_7/ising_model.f90
+++ week_7/ising_model.f90
@@ -17,10 +17,10 @@ interface
     end function
 end interface
 
-double complex, dimension(1:2,1:2) :: sigma_x, sigma_y, sigma_z, mat
+double complex, dimension(1:2,1:2) :: sigma_x, sigma_y, sigma_z
 double complex, dimension(1:2**2,1:2**2) :: sigma_int
 integer ii, jj, NN, allocate_status
-double complex, dimension(:,:), allocatable :: hamiltonian,mean_int,neigh_int,hamiltonian_1
+double complex, dimension(:,:), allocatable :: hamiltonian,mean_int,neigh_int
 real*8 :: lambda
 
 
@@ -51,46 +51,19 @@ allocate(mean_int(1:2**NN,1:2**NN),stat = allocate_status)
 if (allocate_status .ne. 0) stop "***Not enough memory to allocate hamiltonian***"
 allocate(neigh_int(1:2**NN,1:2**NN),stat = allocate_status)
 if (allocate_status .ne. 0) stop "***Not enough memory to allocate hamiltonian***"
-allocate(hamiltonian_1(1:2**NN,1:2**NN),stat = allocate_status)
-if (allocate_status .ne. 0) stop "***Not enough memory to allocate hamiltonian***"
-
 
 
 ! Transverse field interaction
-do ii = 1, NN
-    if (ii.eq.1) then
-        mean_int(1:2,1:2) = sigma_z
-    else
-        mean_int(1:2,1:2) = identity(2)
-    end if
-
-    do jj = 2, NN
-        if (jj.eq.ii) then
-            mat = sigma_z
-        else
-            mat = identity(2)
-        end if
-        mean_int(1:2**jj,1:2**jj) = tens_prod(&
-            mean_int(1:2**(jj-1),1:2**(jj-1)),&
-            mat,&
-            2**(jj-1),2**(jj-1),2,2)
-    end do
-    hamiltonian = hamiltonian + mean_int
-
-end do
-
 ! Setting everything for ii = 1
 mean_int = tens_prod(&
     sigma_z,&
     identity(2**(NN-1)),&
     2,2,2**(NN-1),2**(NN-1))
 
-hamiltonian_1 = hamiltonian_1 + mean_int
+hamiltonian = hamiltonian + mean_int
 
 ! Setting for states inbetween
 do ii = 2, NN-1
-    ! Initialize whole matrix as identity
-    mean_int = identity(2**NN)
     mean_int(1:2**ii,1:2**ii) = tens_prod(&
             identity(2**(ii-1)),&
             sigma_z,&
@@ -100,7 +73,7 @@ do ii = 2, NN-1
     identity(2**(NN-ii)),&
     2**ii,2**ii,2**(NN-ii),2**(NN-ii))
 
-    hamiltonian_1 = hamiltonian_1 + mean_int
+    hamiltonian = hamiltonian + mean_int
 
 end do
 
@@ -110,32 +83,8 @@ mean_int = tens_prod(&
     sigma_z,&
     2**(NN-1),2**(NN-1),2,2)
 
-hamiltonian_1 = hamiltonian_1 + mean_int
-
+hamiltonian = hamiltonian + mean_int
 hamiltonian = lambda*hamiltonian
-hamiltonian_1 = lambda*hamiltonian_1
-
-! Nearest neighbours interaction
-do ii = 1, NN-1
-    if (ii.eq.1) then
-        neigh_int(1:2,1:2) = sigma_x
-    else
-        neigh_int(1:2,1:2) = identity(2)
-    end if
-
-    do jj = 2, NN
-        if ((jj.eq.ii+1).or.(jj.eq.ii)) then
-            mat = sigma_x
-        else
-            mat = identity(2)
-        end if
-        neigh_int(1:2**jj,1:2**jj) = tens_prod(&
-            neigh_int(1:2**(jj-1),1:2**(jj-1)),&
-            mat,&
-            2**(jj-1),2**(jj-1),2,2)
-    end do
-    hamiltonian = hamiltonian + neigh_int
-end do
 
 ! Nearest neighbours interaction
 ! Prepare interaction of the first two
@@ -144,7 +93,7 @@ neigh_int = tens_prod(&
                 identity(2**(NN-2)),&
                 2**2,2**2,2**(NN-2),2**(NN-2))
 
-hamiltonian_1 = hamiltonian_1 + neigh_int
+hamiltonian = hamiltonian + neigh_int
 
 do ii = 2, NN-2
     neigh_int(1:2**(ii+1),1:2**(ii+1)) = tens_prod(&
@@ -155,7 +104,7 @@ do ii = 2, NN-2
                     neigh_int(1:2**(ii+1),1:2**(ii+1)),&
                     identity(2**(NN-ii-1)),&
                     2**(ii+1),2**(ii+1),2**(NN-ii-1),2**(NN-ii-1))
-    hamiltonian_1 = hamiltonian_1 + neigh_int
+    hamiltonian = hamiltonian + neigh_int
 end do
 
 ! Prepare interaction of the last two
@@ -164,23 +113,8 @@ neigh_int = tens_prod(&
                 sigma_int,&
                 2**(NN-2),2**(NN-2),2**2,2**2)
 
-hamiltonian_1 = hamiltonian_1 + neigh_int
+hamiltonian = hamiltonian + neigh_int
 
-!do ii = 1, 2**NN
-!    do jj = 1, 2**NN
-!        if ((real(real(hamiltonian_1(ii,jj))).ne.real(real(hamiltonian(ii,jj))))&
-!        .or.((real(aimag(hamiltonian_1(ii,jj))).ne.real(aimag(hamiltonian(ii,jj))))))&
-!        stop "They're different :("
-!
-!    end do
-!end do
-!
-!
-!do ii = 1, 2**NN
-!    write(*,'(*(F0.0,SP,F0.0,"i",", "))') hamiltonian(ii,:)
-!    write(*,'(*(F0.0,SP,F0.0,"i",", "))') hamiltonian_1(ii,:)
-!    write(*,*) ""
-!end do
 
 end program ising_model
 
