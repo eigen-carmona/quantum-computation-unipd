1c74e5af7b1f0ac24f6d5cf1fc374399919d9e4f
eigen-carmona <jgerardocarg96@gmail.com>
2021-12-16 14:24:33 +0100

    Added third method. Accurately reproduces results

--- week_7/ising_model.f90
+++ week_7/ising_model.f90
@@ -1,6 +1,44 @@
 module ising_functs
 contains
 
+subroutine print_mat(mat_a)
+implicit none
+double complex, dimension(:,:) :: mat_a
+integer :: ii, shape_a(1:2)
+shape_a = shape(mat_a)
+do ii = 1, shape_a(1)
+    write(*,'(*(F0.0,SP,F0.0,"i",","))') mat_a(ii,:)
+end do
+write(*,*) "---------------------------------------"
+end subroutine
+
+subroutine compare_mat(mat_a,mat_b)
+implicit none
+double complex, dimension(:,:) :: mat_a,mat_b
+integer :: ii, jj, shape_a(1:2), shape_b(1:2)
+    shape_a = shape(mat_a)
+    shape_b = shape(mat_b)
+    do ii = 1,2
+        if (shape_a(ii).ne.shape_b(ii)) stop "Matrices differ in shape"
+    end do
+    do jj = 1, shape_a(2)
+        do ii = 1, shape_a(1)
+            if(&
+                (real(real(mat_a(ii,jj))).ne.real(real(mat_b(ii,jj))))&
+                .or.&
+                (real(aimag(mat_a(ii,jj))).ne.real(aimag(mat_b(ii,jj))))&
+                ) then
+                write(*,'(A,I0)') "matrix A, row ", ii
+                call print_mat(mat_a(ii:ii,:))
+                write(*,'(A,I0)') "matrix B, row ", ii
+                call print_mat(mat_b(ii:ii,:))
+                write(*,'(A,I0)') "different in element ",jj
+                stop
+            end if
+        end do
+    end do    
+end subroutine
+
 function tens_prod(A,B,MM,NN,OO,PP)
 implicit none
 double complex, dimension(:,:) :: A,B
@@ -34,7 +72,7 @@ implicit none
 integer :: OO, ii
 double complex :: val, diag_val(1:OO,1:OO)
 
-    !diag_val = 0
+    diag_val = 0
     !if (val.eq.dcmplx(0,0)) return
     do ii = 1, OO
         diag_val(ii,ii) = val
@@ -100,9 +138,9 @@ allocate(neigh_int(1:2**NN,1:2**NN),stat = allocate_status)
 if (allocate_status .ne. 0) stop "***Not enough memory to allocate hamiltonian***"
 
 
-call cpu_time(start)
-!
-do jj = 1, 100
+!call cpu_time(start)
+!!
+!do jj = 1, 100
 ! Transverse field interaction
 ! Setting everything for ii = 1
 mean_int = tens_prod(&
@@ -136,18 +174,43 @@ mean_int = tens_prod(&
 
 hamiltonian = hamiltonian + mean_int
 
+
+! Nearest neighbours interaction
+! Prepare interaction of the first two
+neigh_int = tens_prod(&
+                sigma_int,&
+                identity(2**(NN-2)),&
+                2**2,2**2,2**(NN-2),2**(NN-2))
+
+hamiltonian = hamiltonian + neigh_int
+
+do ii = 2, NN-2
+    neigh_int(1:2**(ii+1),1:2**(ii+1)) = tens_prod(&
+                    identity(2**(ii-1)),&
+                    sigma_int,&
+                    2**(ii-1),2**(ii-1),2**2,2**2)
+    neigh_int = tens_prod(&
+                    neigh_int(1:2**(ii+1),1:2**(ii+1)),&
+                    identity(2**(NN-ii-1)),&
+                    2**(ii+1),2**(ii+1),2**(NN-ii-1),2**(NN-ii-1))
+    hamiltonian = hamiltonian + neigh_int
 end do
-call cpu_time(end)
-write(*,*) "First method timing: ", (end-start)
 
+! Prepare interaction of the last two
+neigh_int = tens_prod(&
+                identity(2**(NN-2)),&
+                sigma_int,&
+                2**(NN-2),2**(NN-2),2**2,2**2)
 
-! *** ALTERNATIVE ***
+hamiltonian = hamiltonian + neigh_int
+
+! **** ALTERNATIVE ****
  
 allocate(hamiltonian_1(1:2**NN,1:2**NN),stat = allocate_status)
 if (allocate_status .ne. 0) stop "***Not enough memory to allocate hamiltonian***"
 
-call cpu_time(start)
-do jj = 1, 100
+!call cpu_time(start)
+!do jj = 1, 100
 ! Transverse field interaction
 ! Setting everything for ii = 1
 mean_int = tens_id(&
@@ -179,42 +242,23 @@ mean_int = tens_prod(&
 
 hamiltonian_1 = hamiltonian_1 + mean_int
 
-end do
-call cpu_time(end)
-write(*,*) "Second method timing: ", (end-start)
-
-
-do ii = 1, 2**NN
-    do jj = 1, 2**NN
-        if(&
-            (real(real(hamiltonian(ii,jj))).ne.real(real(hamiltonian_1(ii,jj))))&
-            .or.&
-            (real(aimag(hamiltonian(ii,jj))).ne.real(aimag(hamiltonian_1(ii,jj))))&
-            ) stop "They are different :("
-    end do
-end do
-
-hamiltonian = lambda*hamiltonian
-
 ! Nearest neighbours interaction
 ! Prepare interaction of the first two
-neigh_int = tens_prod(&
+neigh_int = tens_id(&
                 sigma_int,&
-                identity(2**(NN-2)),&
-                2**2,2**2,2**(NN-2),2**(NN-2))
+                2**2,2**2,2**(NN-2))
 
-hamiltonian = hamiltonian + neigh_int
+hamiltonian_1 = hamiltonian_1 + neigh_int
 
 do ii = 2, NN-2
     neigh_int(1:2**(ii+1),1:2**(ii+1)) = tens_prod(&
                     identity(2**(ii-1)),&
                     sigma_int,&
                     2**(ii-1),2**(ii-1),2**2,2**2)
-    neigh_int = tens_prod(&
+    neigh_int = tens_id(&
                     neigh_int(1:2**(ii+1),1:2**(ii+1)),&
-                    identity(2**(NN-ii-1)),&
-                    2**(ii+1),2**(ii+1),2**(NN-ii-1),2**(NN-ii-1))
-    hamiltonian = hamiltonian + neigh_int
+                    2**(ii+1),2**(ii+1),2**(NN-ii-1))
+    hamiltonian_1 = hamiltonian_1 + neigh_int
 end do
 
 ! Prepare interaction of the last two
@@ -223,7 +267,8 @@ neigh_int = tens_prod(&
                 sigma_int,&
                 2**(NN-2),2**(NN-2),2**2,2**2)
 
-hamiltonian = hamiltonian + neigh_int
+hamiltonian_1 = hamiltonian_1 + neigh_int
 
+call compare_mat(hamiltonian,hamiltonian_1)
 
 end program ising_model
